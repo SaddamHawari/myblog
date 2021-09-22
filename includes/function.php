31c4cdabfbf4f82f9ac92f5aabd574ffda2560f0
Saddam Hawari <79617383+h2hcrew@users.noreply.github.com>
2021-09-22 18:48:49 +0545

    Add files via upload

---
+++ includes/function.php
@@ -0,0 +1,9 @@
+<?php
+
+function getCategory($db,$id){
+    $query="SELECT * FROM category WHERE id=$id";
+    $run=mysqli_query($db,$query);
+    $data=mysqli_fetch_assoc($run);
+    return $data['name'];
+
+}
